c47e1343d94e6e2b07ef92df4078289c4f92f5fa
Mariya <lomakina.ms@gmail.com>
2024-04-15 01:28:13 +0300

    Добавила выбор эмодзи и цвета

--- Tracker/Extension/Colors.swift
+++ Tracker/Extension/Colors.swift
@@ -10,22 +10,22 @@ extension UIColor {
     static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
     static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
     static var ypLightGray: UIColor { UIColor(named: "YP Light Gray") ?? UIColor.lightGray }
-    static var ColorSelection1: UIColor { UIColor(named: "Color selection 1")!}
-    static var ColorSelection2: UIColor { UIColor(named: "Color selection 2")!}
-    static var ColorSelection3: UIColor { UIColor(named: "Color selection 3")!}
-    static var ColorSelection4: UIColor { UIColor(named: "Color selection 4")!}
-    static var ColorSelection5: UIColor { UIColor(named: "Color selection 5")!}
-    static var ColorSelection6: UIColor { UIColor(named: "Color selection 6")!}
-    static var ColorSelection7: UIColor { UIColor(named: "Color selection 7")!}
-    static var ColorSelection8: UIColor { UIColor(named: "Color selection 8")!}
-    static var ColorSelection9: UIColor { UIColor(named: "Color selection 9")!}
-    static var ColorSelection10: UIColor { UIColor(named: "Color selection 10")!}
-    static var ColorSelection11: UIColor { UIColor(named: "Color selection 11")!}
-    static var ColorSelection12: UIColor { UIColor(named: "Color selection 12")!}
-    static var ColorSelection13: UIColor { UIColor(named: "Color selection 13")!}
-    static var ColorSelection14: UIColor { UIColor(named: "Color selection 14")!}
-    static var ColorSelection15: UIColor { UIColor(named: "Color selection 15")!}
-    static var ColorSelection16: UIColor { UIColor(named: "Color selection 16")!}
-    static var ColorSelection17: UIColor { UIColor(named: "Color selection 17")!}
-    static var ColorSelection18: UIColor { UIColor(named: "Color selection 18")!}
+    static var CS1: UIColor { UIColor(named: "Color selection 1")!}
+    static var CS2: UIColor { UIColor(named: "Color selection 2")!}
+    static var CS3: UIColor { UIColor(named: "Color selection 3")!}
+    static var CS4: UIColor { UIColor(named: "Color selection 4")!}
+    static var CS5: UIColor { UIColor(named: "Color selection 5")!}
+    static var CS6: UIColor { UIColor(named: "Color selection 6")!}
+    static var CS7: UIColor { UIColor(named: "Color selection 7")!}
+    static var CS8: UIColor { UIColor(named: "Color selection 8")!}
+    static var CS9: UIColor { UIColor(named: "Color selection 9")!}
+    static var CS10: UIColor { UIColor(named: "Color selection 10")!}
+    static var CS11: UIColor { UIColor(named: "Color selection 11")!}
+    static var CS12: UIColor { UIColor(named: "Color selection 12")!}
+    static var CS13: UIColor { UIColor(named: "Color selection 13")!}
+    static var CS14: UIColor { UIColor(named: "Color selection 14")!}
+    static var CS15: UIColor { UIColor(named: "Color selection 15")!}
+    static var CS16: UIColor { UIColor(named: "Color selection 16")!}
+    static var CS17: UIColor { UIColor(named: "Color selection 17")!}
+    static var CS18: UIColor { UIColor(named: "Color selection 18")!}
 }
--- Tracker/ViewControllers/NewHabitOrEventViewController/NewHabitOrEventViewController.swift
+++ Tracker/ViewControllers/NewHabitOrEventViewController/NewHabitOrEventViewController.swift
@@ -6,8 +6,32 @@ protocol NewHabitOrEventViewControllerDelegate: AnyObject {
 }
 
 final class NewHabitOrEventViewController: UIViewController {
+    //MARK: - Private struct
+    private struct CollectionParams {
+        let cellCount: Int
+        let height: CGFloat
+        let leftInset: CGFloat
+        let rightInset: CGFloat
+        let cellSpacing: CGFloat
+        
+        init(cellCount: Int, height: CGFloat, leftInset: CGFloat, rightInset: CGFloat, cellSpacing: CGFloat) {
+            self.cellCount = cellCount
+            self.height = height
+            self.leftInset = leftInset
+            self.rightInset = rightInset
+            self.cellSpacing = cellSpacing
+        }
+    }
     
-    //MARK: - Properties
+    private let collParams = CollectionParams(
+        cellCount: 6,
+        height: 204,
+        leftInset: 18,
+        rightInset: 18,
+        cellSpacing: 5
+    )
+    
+    //MARK: - View
     private  lazy var trackerNameInput: UITextField = {
         let textField = UITextField()
         textField.textColor = .ypBlack
@@ -76,6 +100,57 @@ final class NewHabitOrEventViewController: UIViewController {
         return tableView
     }()
     
+    private let emojiLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Emoji"
+        label.textColor = .ypBlack
+        label.font = .systemFont(ofSize: 19, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var emojiCollection: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(EmojiCollectionCell.self, forCellWithReuseIdentifier: "emojiCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private let colorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Цвет"
+        label.textColor = .ypBlack
+        label.font = .systemFont(ofSize: 19, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var colorCollection: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(ColorCollectionCell.self, forCellWithReuseIdentifier: "colorCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.delaysContentTouches = false
+        return scrollView
+    }()
+
+    private let contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = .ypWhite
+        return contentView
+    }()
+    
+    
+    //MARK: - Properties
+    private let colorArray: [UIColor] = [.CS1, .CS2, .CS3, .CS4, .CS5, .CS6, .CS7, .CS8, .CS9, .CS10, .CS11, .CS12, .CS13, .CS14, .CS15, .CS16, .CS17, .CS18]
+    private let emojiArray: [String] = ["🙂", "😻", "🌺", "🐶", "❤️", "😱", "😇", "😡", "🥶", "🤔", "🙌", "🍔", "🥦", "🏓", "🥇", "🎸", "🏝️", "😪"]
     private var isTrackerNameFilled: Bool = false
     private var categoryTitle: String?
     private var color: UIColor?
@@ -96,43 +171,88 @@ final class NewHabitOrEventViewController: UIViewController {
         addTapGestureToHideKeyboard()
         trackerProperties.dataSource = self
         trackerProperties.delegate = self
+        emojiCollection.dataSource = self
+        emojiCollection.delegate = self
+        colorCollection.dataSource = self
+        colorCollection.delegate = self
+        emojiCollection.allowsMultipleSelection = false
+        colorCollection.allowsMultipleSelection = false
     }
     
     // MARK: - Private Function
     private func addElements(){
-        view.addSubview(trackerNameInput)
-        view.addSubview(restrictiveLabel)
-        view.addSubview(cancelButton)
-        view.addSubview(createButton)
-        view.addSubview(trackerProperties)
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(trackerNameInput)
+        contentView.addSubview(restrictiveLabel)
+        contentView.addSubview(trackerProperties)
+        contentView.addSubview(emojiLabel)
+        contentView.addSubview(emojiCollection)
+        contentView.addSubview(colorCollection)
+        contentView.addSubview(colorLabel)
+        contentView.addSubview(cancelButton)
+        contentView.addSubview(createButton)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            trackerNameInput.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            trackerNameInput.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            trackerNameInput.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            trackerNameInput.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 16),
+            trackerNameInput.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            trackerNameInput.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             trackerNameInput.heightAnchor.constraint(equalToConstant: 75),
             
-            restrictiveLabel.topAnchor.constraint(equalTo: trackerNameInput.bottomAnchor, constant: 8),
+            restrictiveLabel.topAnchor.constraint(equalTo: trackerNameInput.bottomAnchor),
             restrictiveLabel.leadingAnchor.constraint(equalTo: trackerNameInput.leadingAnchor, constant: 28),
             restrictiveLabel.trailingAnchor.constraint(equalTo: trackerNameInput.trailingAnchor, constant: -28),
             restrictiveLabel.heightAnchor.constraint(equalToConstant: 22),
             
-            createButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            createButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            createButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             createButton.widthAnchor.constraint(equalTo: cancelButton.widthAnchor),
             createButton.heightAnchor.constraint(equalToConstant: 60),
+            createButton.topAnchor.constraint(equalTo: colorCollection.bottomAnchor, constant: 16),
+            createButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
             
-            cancelButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            cancelButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             cancelButton.rightAnchor.constraint(equalTo: createButton.leftAnchor, constant: -8),
             cancelButton.heightAnchor.constraint(equalToConstant: 60),
+            cancelButton.topAnchor.constraint(equalTo: colorCollection.bottomAnchor, constant: 16),
+            cancelButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
             
-            trackerProperties.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            trackerProperties.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            trackerProperties.topAnchor.constraint(equalTo: restrictiveLabel.bottomAnchor, constant: 24),
-            trackerProperties.heightAnchor.constraint(equalToConstant: eventMode ? 75 : 150)
+            trackerProperties.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            trackerProperties.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            trackerProperties.topAnchor.constraint(equalTo: trackerNameInput.bottomAnchor, constant: 24),
+            trackerProperties.heightAnchor.constraint(equalToConstant: eventMode ? 75 : 150),
+            
+            emojiLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 28),
+            emojiLabel.topAnchor.constraint(equalTo: trackerProperties.bottomAnchor, constant: 32),
+            emojiLabel.widthAnchor.constraint(equalToConstant: 52),
+            emojiLabel.heightAnchor.constraint(equalToConstant: 18),
+            
+            emojiCollection.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            emojiCollection.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            emojiCollection.topAnchor.constraint(equalTo: emojiLabel.bottomAnchor),
+            emojiCollection.heightAnchor.constraint(equalToConstant: collParams.height),
+            
+            colorLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 28),
+            colorLabel.topAnchor.constraint(equalTo: emojiCollection.bottomAnchor, constant: 16),
+            colorLabel.widthAnchor.constraint(equalToConstant: 48),
+            colorLabel.heightAnchor.constraint(equalToConstant: 18),
+            
+            colorCollection.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            colorCollection.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            colorCollection.topAnchor.constraint(equalTo: colorLabel.bottomAnchor),
+            colorCollection.heightAnchor.constraint(equalToConstant: collParams.height),
+            
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
         ])
     }
     
@@ -161,7 +281,7 @@ final class NewHabitOrEventViewController: UIViewController {
         let newTracker = Tracker(
             id: UUID(),
             name: trackerNameInput.text ?? "",
-            color: self.color ?? .ColorSelection17,
+            color: self.color ?? .CS17,
             emoji: self.emoji ?? "❤️",
             schedule: self.schedule)
         let category = TrackerCategory(
@@ -248,3 +368,68 @@ extension NewHabitOrEventViewController: ScheduleViewControllerDelegate {
         checkFullFill()
     }
 }
+
+// MARK: - Extension UICollectionViewDataSource
+extension NewHabitOrEventViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colorArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == emojiCollection {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "emojiCell", for: indexPath) as? EmojiCollectionCell else { return UICollectionViewCell() }
+            let emoji = emojiArray[indexPath.row]
+            cell.configure(emoji: emoji)
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath) as? ColorCollectionCell else { return UICollectionViewCell() }
+            let color = colorArray[indexPath.row]
+            cell.configure(color: color)
+            return cell
+        }
+    }
+}
+
+// MARK: - Extension UICollectionViewDelegate
+extension NewHabitOrEventViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 24, left: collParams.leftInset, bottom: 24, right: collParams.rightInset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingWidth: CGFloat = collParams.leftInset + collParams.rightInset + CGFloat((collParams.cellCount - 1)) * collParams.cellSpacing
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let cellWidth =  availableWidth / CGFloat(collParams.cellCount)
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return collParams.cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        if collectionView == emojiCollection {
+            emoji = emojiArray[indexPath.row]
+            cell?.contentView.backgroundColor = .ypLightGray
+        } else {
+            color = colorArray[indexPath.row]
+            cell?.contentView.layer.borderWidth = 3
+            cell?.contentView.layer.borderColor = UIColor.ypGreen.withAlphaComponent(0.3).cgColor
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        if collectionView == emojiCollection {
+            cell?.contentView.backgroundColor = .ypWhite
+        } else {
+            color = colorArray[indexPath.row]
+            cell?.contentView.layer.borderWidth = 0
+            cell?.contentView.layer.borderColor = UIColor.ypGreen.withAlphaComponent(0.3).cgColor
+        }
+    }
+    
+}
+
