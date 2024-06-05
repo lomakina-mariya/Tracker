0f1b9cf690a82c7a973a47f931929dca525cb0f1
Mariya <lomakina.ms@gmail.com>
2024-06-05 10:37:35 +0300

    Убрала pods из .gitignore

--- Tracker/Extension/IntForDays.swift
+++ Tracker/Extension/IntForDays.swift
@@ -7,13 +7,13 @@ extension Int {
         let remainder100 = self % 100
         
         if self == 1 {
-            return "\(self) \("day1".localized)"
+            return "\(self) \("oneDay".localized)"
         } else if remainder10 == 1 && remainder100 != 11 {
-            return "\(self) \("day0".localized)"
+            return "\(self) \("numberEndingOneWithoutEleven".localized)"
         } else if remainder10 >= 2 && remainder10 <= 4 && (remainder100 < 10 || remainder100 >= 20) {
-            return "\(self) \("day2".localized)"
+            return "\(self) \("numberEndingFromTwoToFour".localized)"
         } else {
-            return "\(self) \("days".localized)"
+            return "\(self) \("otherDaysNumber".localized)"
         }
     }
 }
